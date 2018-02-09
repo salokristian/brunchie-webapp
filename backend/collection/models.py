2e2de3deee776723a322623e8c9514ea02315bdb
Kristian Salo <salok2@aalto.fi>
2018-02-09 20:31:22 +0200

    Added POST for all restaurants.

--- backend/collection/models.py
+++ backend/collection/models.py
@@ -2,11 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Restaurant(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     score = models.IntegerField() #maybe create a review class
     review = models.TextField()
 
     def __str__(self):
         return self.name
-
-
--- backend/collection/views.py
+++ backend/collection/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from collection.models import Restaurant
 from collection.serializers import RestaurantSerializer
 
+
+
 @api_view(['GET', 'POST'])
 def restaurant_list(request):
     """
@@ -14,6 +16,16 @@ def restaurant_list(request):
         restaurants = Restaurant.objects.all()
         serializer = RestaurantSerializer(restaurants, many=True)
         return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = RestaurantSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def restaurant_details(request, pk):
