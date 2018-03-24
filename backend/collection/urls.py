4531b5f2e299a6258284e2522562b9527c9a23a8
Kristian Salo <salo.kristian@hotmail.com>
2018-03-24 10:46:00 +0200

    Refactored the views from function-based to class-based.

--- backend/collection/urls.py
+++ backend/collection/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from collection import views
 
 urlpatterns = [
-    path('restaurants/', views.restaurant_list, name="get_post_restaurants"),
-    path('restaurants/<int:pk>/', views.restaurant_details, name="get_put_delete_restaurant")
+    path('restaurants/', views.restaurant_list.as_view(), name="get_post_restaurants"),
+    path('restaurants/<int:pk>/', views.restaurant_details.as_view(), name="get_put_delete_restaurant")
 ]
--- backend/collection/views.py
+++ backend/collection/views.py
@@ -1,51 +1,19 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import generics
 from collection.models import Restaurant
 from collection.serializers import RestaurantSerializer
 
 
-
-@api_view(['GET', 'POST'])
-def restaurant_list(request):
+class restaurant_list(generics.ListCreateAPIView):
     """
     List all restaurants or create a new one.
     """
-
-    if request.method == 'GET':
-        restaurants = Restaurant.objects.all()
-        serializer = RestaurantSerializer(restaurants, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = RestaurantSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
 
 
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def restaurant_details(request, pk):
+class restaurant_details(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a restaurant.
     """
-
-    try:
-        restaurant = Restaurant.objects.get(pk=pk)
-    except Restaurant.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = RestaurantSerializer(restaurant)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = RestaurantSerializer(restaurant, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
