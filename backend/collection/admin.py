e89cb07a03933d3acf7ebe717738ab097920f810
Kristian Salo <salo.kristian@hotmail.com>
2018-04-05 15:42:55 +0300

    Migrated the model changes and added the new models to the admin page.

--- backend/collection/admin.py
+++ backend/collection/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from collection.models import Restaurant
+from collection.models import *
 
 # Register your models here.
 admin.site.register(Restaurant)
+admin.site.register(Serving)
+admin.site.register(AlaCarteDish)
+admin.site.register(BuffetMenu)
+admin.site.register(OccursAt)
+admin.site.register(Review)
